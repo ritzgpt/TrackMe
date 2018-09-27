a3ab2c62fface76f4046cfcaa96824883eb70074
ritesh gupta <ritesh.gupta2@freschesolutions.com>
2018-09-27 12:55:41 +0530

    basic list operation

---
+++ basics/practice/ListOperation.java
@@ -0,0 +1,42 @@
+package basics.practice;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/*
+List Operations using Java8 Features
+ */
+public class ListOperation {
+    public static void main(String args[]) {
+        findRecord();
+    }
+    static void findRecord(){
+        List<String> strList=new ArrayList<>();
+        strList.add("A");
+        strList.add("B");
+        strList.add("C");
+        strList.add("D");
+        strList.add("E");
+        strList.add("F");
+        strList.add("G");
+        strList.add("H");
+        strList.add("I");
+        strList.add("J");
+
+
+        List<String> strList2=new ArrayList<>();
+        strList2.add("A");
+        strList2.add("B");
+        strList2.add("C1");
+        strList2.add("D2");
+        strList2.add("E2");
+        strList2.add("F2");
+        strList2.add("G2");
+        strList2.add("H2");
+        strList2.add("I2");
+        strList2.add("J2");
+
+        strList2.forEach(System.out::println);
+
+    }
+}
